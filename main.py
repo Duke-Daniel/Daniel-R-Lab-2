e29734a38b7ed85ae745c66f9f0a94c9372b575a
Duke-Daniel <drecord29@setoncatholic.org>
2026-09-22 20:11:21 +0000

    Random guess completed

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-x = 6
-if x < 5:
-    print("hello")
-elif x > 5:
-    print("goodbye")
-else:
-    print("stinky")
+import random
+
+
+game_number = random.randint(1,10)
+print(game_number)
+while(True):
+    guess = int(input("guess a number between 1 and 10:"))
+    if guess > game_number:
+        print("To high")
+    elif guess < game_number:
+        print("To low")
+    else:
+        print("You Win")
+        break
