8961c24c80d23cc2cb36cd340357d72b7d8f7cd6
Duke-Daniel <drecord29@setoncatholic.org>
2026-09-22 20:22:50 +0000

    guessing game complete

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import random
 
 
 game_number = random.randint(1,10)
-print(game_number)
+#print(game_number)
+guess_count = 0
+
 while(True):
     guess = int(input("guess a number between 1 and 10:"))
+    guess_count += 1
     if guess > game_number:
         print("To high")
     elif guess < game_number:
         print("To low")
     else:
-        print("You Win")
+        print(f"You Win! It took you {guess_count} guesses.")
+        if (guess_count) <= 3:
+            print("Great guessing!")
+        else:
+            print("Try to guess the number in fewer attempts next time.")
         break
+    
